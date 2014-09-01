f43b358815e10ce8ff95a0e1de32e26501a1b1e0
David Herron <david@davidherron.com>
2014-09-01 15:15:09 -0700

    Update rot13.js

--- rot13.js
+++ rot13.js
@@ -7,7 +7,7 @@ var result = 'nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM';
 var srcstr = new ss(src);
 var ret    = new ss('');
 
-rot13encode(srcstr, ret);
+r13.rot13encode(srcstr, ret);
 
 ret.on('finish', function() {
   console.log('Expected: '+ result);
